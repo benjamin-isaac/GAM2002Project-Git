ecbd0e9a9f2acf13362cd3cfc5e71890d3a2eb71
benjamin <bm816@live.mdx.ac.uk>
2021-11-24 17:53:56 +0000

    MVP story quest scripted and initialised

---
+++ Assets/scripts/QuestGiver.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public Player player;
+
+    public GameObject questWindow;
+    public Text titleText;
+    public Text descriptionText;
+    public Text experienceText;
+    public void OpenQuestWindow()
+    {
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+        experienceText.text = quest.experienceReward.ToString();
+    }
+
+    public void AcceptQuest()
+    {
+        questWindow.SetActive(false);
+        quest.isActive = true;
+        player.quest = quest;
+    }
+
+}
